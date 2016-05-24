c72a215b9a77a38cfc94a23af323e0e997206d76
Wolfie5493 <lonewolfwanderer@gmail.com>
2016-05-25 00:49:29 +0200

    Organized things

---
+++ 2016/StudentAppFiles/ReadMe.txt
@@ -0,0 +1,1 @@
+Just a simple program to gather a students information, and then store it to a text file.
--- 2016/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "addExample.h"
- 
-int main()
-{
-    using namespace std;
-    cout << "The sum of 3 and 4 is " << add(3, 4) << endl;
-    return 0;
-}
